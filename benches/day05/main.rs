8e151c681f6e361dadbd598631733943a36af6cb
Emma Böcker <stckoverflw@gmail.com>
2024-12-06 14:06:36 +0100

    add day 5 to 7

---
+++ benches/day05/main.rs
@@ -0,0 +1,12 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+#[path = "../../src/bin/day05.rs"]
+mod day05;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("day 05", |b| b.iter(day05::main));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
+
